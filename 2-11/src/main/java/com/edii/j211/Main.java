6d29b8c8519f0954cdaecd2045167b796be486ab
Lauri <lauri.paatelainen@gmail.com>
2017-03-16 17:49:59 +0200

    Projektin pystytystä ja pitin testausta

---
+++ 2-11/src/main/java/com/edii/j211/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.edii.j211;
+
+/**
+ *
+ * @author edii
+ */
+public class Main {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            args[0] = "Testi";
+            System.out.println("Testi");
+        }
+    }
+}
